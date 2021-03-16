0ffbd227527b6913a2add257084c2c2e23e06574
wuyuanyi <wuyuanyi135@gmail.com>
2021-03-16 03:22:02 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="jupyter_cell_functionizer",
-    version="0.1",
+    version="0.2",
     packages=find_packages(),
     author="Yuanyi Wu",
     url="https://github.com/wuyuanyi135/jupyter_cell_functionizer"
